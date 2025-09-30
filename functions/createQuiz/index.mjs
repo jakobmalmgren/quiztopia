aac840a217f8bbd91d1484ba87235d9470e571b2
Jakob Malmgren <Jakob.Malmgren1987@gmail.com>
2025-09-30 16:13:57 +0200

    gjort så man kan logga in o man verifierar sig och får token samt o skapa quiz för användare

--- functions/createQuiz/index.mjs
+++ functions/createQuiz/index.mjs
@@ -3,13 +3,50 @@ import middy from "@middy/core";
 import { transpileSchema } from "@middy/validator/transpile";
 import validator from "@middy/validator";
 import { client } from "../../utils/dbClient.mjs";
+import { PutItemCommand } from "@aws-sdk/client-dynamodb";
+import createError from "http-errors";
 import { createQuizSchema } from "../../schemas/createQuizSchema.mjs";
+import { errorHandler } from "../../middlewares/errorHandler.mjs";
+import { verifyToken } from "../../middlewares/authHandler.mjs";
+import { v4 as uuidv4 } from "uuid";
 
-export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
-    }),
-  };
+const createQuizHandler = async (event) => {
+  try {
+    //checkar me mitt validatorschema så de finns och hur de ska skrivas
+    const { quizName } = event.body;
+    console.log("EVENTTT!!!!:", event.user);
+    const id = uuidv4();
+
+    const putItemCommand = new PutItemCommand({
+      TableName: "QuizTable",
+      Item: {
+        pk: { S: event.user.userId },
+        sk: { S: `QUIZ#${id}` },
+        entityType: { S: "QUIZ" },
+        quizName: { S: quizName },
+        creator: { S: event.user.userName },
+      },
+    });
+
+    await client.send(putItemCommand);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "quiz added successfully!",
+      }),
+    };
+  } catch (error) {
+    if (createError.isHttpError(error)) {
+      throw error;
+    } else {
+      throw new createError.InternalServerError("failed to add a quiz!");
+    }
+  }
 };
+
+export const handler = middy(createQuizHandler)
+  .use(verifyToken())
+  .use(httpJsonBodyParser())
+  .use(validator({ eventSchema: transpileSchema(createQuizSchema) }))
+  .use(errorHandler());
--- middlewares/authHandler.mjs
+++ middlewares/authHandler.mjs
@@ -1,24 +1,39 @@
 import createError from "http-errors";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-export const verifyToken = (event) => {
-  const authHeader = event.headers?.authorization;
-  const token = authHeader.replace("Bearer ", "");
-  if (!token) {
-    throw new createError.Unauthorized("Missing token");
-  }
-  try {
-    return jwt.verify(token, process.env.MY_SECRET_KEY);
-  } catch (error) {
-    // catch (error) {
-    //   console.log("errorrr:", error);
-    //   throw createError.InternalServerError("Something went wrong");
-    // }
-    if (createError.isHttpError(error)) {
-      throw error;
-    } else {
-      throw new createError.InternalServerError(
-        `something went wrong!: ${error.message}`
-      );
-    }
-  }
+dotenv.config();
+
+export const verifyToken = () => {
+  return {
+    before: async (request) => {
+      const authHeader = request.event.headers?.Authorization;
+      if (!authHeader) {
+        throw new createError.Unauthorized("Missing token");
+      }
+      const token = authHeader.replace("Bearer ", "");
+      // kolla om ja måste ha de..för måstte väl kolla me
+      // OM DE FINNS ME SÅ DE E FEL TOKEN!!!!!!!!!!!!!!!!!!!!!!!
+      // if (!token) {
+      //   throw new createError.Unauthorized("Missing token");
+      // }
+      try {
+        // måste kolla upp!. kan använda detta senare i mina lambda funkitoner för nå de!
+        request.event.user = jwt.verify(token, process.env.MY_SECRET_KEY);
+      } catch (error) {
+        // catch (error) {
+        //   console.log("errorrr:", error);
+        //   throw createError.InternalServerError("Something went wrong");
+        // }
+        if (createError.isHttpError(error)) {
+          throw error;
+        } else {
+          //ändra som de andra!!!!!!!!!!!!!!!!!
+          throw new createError.InternalServerError(
+            `something went wrong!: ${error.message}`
+          );
+        }
+      }
+    },
+  };
 };
